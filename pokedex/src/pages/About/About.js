e3114fd1f9688a89fa5c1f80310cb4d6c0282d59
brunavabs <bru.almeida12@gmail.com>
2023-08-27 21:20:08 -0300

    projeto final

---
+++ pokedex/src/pages/About/About.js
@@ -0,0 +1,25 @@
+import Header from "../../components/Header/Header";
+import { MainContainer } from "../../constants/stylePages";
+import Footer from "../../components/Footer/Footer";
+import { SectionAbout } from "../../constants/stylePages";
+
+function About() {
+
+    return (
+      <> 
+        <Header/>
+
+          <MainContainer>
+          
+          <SectionAbout >
+
+          </SectionAbout>  
+        </MainContainer>
+        <Footer/>
+      </>
+    );
+  }
+
+  export default About
+  
+  
